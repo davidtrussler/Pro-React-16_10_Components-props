2d0867a9c8fdd716c85270a57c4233d169cc87db
David Trussler <mail@davidtrussler.net>
2020-03-29 21:05:38 +0100

    Adds child components to main component

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'; 
+import Message from './Message'; 
+import Summary from './Summary'; 
 
 export default function App() {
   return (
-    <h1 className="bg-primary text-white text-center p-2">
-      Hello David
-    </h1>
+  	<div>
+	    <h1 className="bg-primary text-white text-center p-2">
+	      Hello David
+	    </h1>
+	    <Message />
+	    <Message />
+	    <Message />
+	    <Summary />
+    </div>
   ); 
 }
