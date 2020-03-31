c7fe6af8a72604eccd86fa47736822ce8e168704
David Trussler <mail@davidtrussler.net>
2020-03-31 20:54:25 +0100

    Renders no content

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,11 +1,15 @@
 import React from 'react'; 
 
 export default function Summary(props) {
-	return (
-		<React.Fragment>  	
-			<td>{props.index + 1}</td>
-			<td>{props.name}</td>
-			<td>{props.name.length}</td>
-		</React.Fragment>
-	); 
+	if (props.name.length >= 4) {
+		return (
+			<React.Fragment>  	
+				<td>{props.index + 1}</td>
+				<td>{props.name}</td>
+				<td>{props.name.length}</td>
+			</React.Fragment>
+		); 
+	} else {
+		return null; 
+	}
 }
